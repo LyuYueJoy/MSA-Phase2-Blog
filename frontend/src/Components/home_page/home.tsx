709397cceaea8409d08ea5e205a400d1529be7d7
LyuYueJoy <112316573+LyuYueJoy@users.noreply.github.com>
2024-07-31 18:47:28 +1200

    homepage

--- frontend/src/Components/home_page/home.tsx
+++ frontend/src/Components/home_page/home.tsx
@@ -1,28 +1,49 @@
-import React from 'react';
-import { Button, Container, Grid, Typography, Paper } from '@mui/material';
-
+import React from "react";
+import { Button, Container, Grid, Typography, Paper, Avatar } from "@mui/material";
 const Home: React.FC = () => {
   return (
     <Container maxWidth="lg" sx={{ my: 4 }}>
       <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <Paper elevation={3} sx={{ p: 2, mb: 4 }}>
+            <img
+              src="wallpaper.jpg"
+              alt="wallpaper"
+              style={{ width: '100%', height: 'auto', borderRadius: '8px' }} // 适当的样式
+            />
+          </Paper>
+        </Grid>
         <Grid item xs={12} md={8}>
           <Paper elevation={3} sx={{ p: 4 }}>
             <Typography variant="h2" component="h1" gutterBottom>
-              Welcome to my blog
+              Hi, Welcome to my blog
             </Typography>
-            
+
+            <Typography variant="h4" component="h2" gutterBottom>
+              My name is JoyLyuYue,
+            </Typography>
+
+            <Typography variant="h6" component="h2" gutterBottom>
+              I am currently a junior student at Auckland University.
+            </Typography>
+
+            <Typography variant="h6" component="h2" gutterBottom>
+              I plan to use my blog to document my interests and maintain my study notes.
+            </Typography>
           </Paper>
         </Grid>
         <Grid item xs={12} md={4}>
-          <Paper elevation={3} sx={{ p: 4 }}>
-            <Typography variant="body1">
-              aaa aaa
-            </Typography>
+        <Paper elevation={3} sx={{ p: 4 }}>
+            <img
+              src="avatar.jpg" 
+              alt="avatar"
+              style={{ width: '100%', height: 'auto', borderRadius: '8px' }} 
+            />
           </Paper>
         </Grid>
       </Grid>
     </Container>
   );
-}
+};
 
 export default Home;
